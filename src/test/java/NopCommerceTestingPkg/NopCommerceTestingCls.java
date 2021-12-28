9ffd83a945b2436d4578c8506e3c29a240bf2eaf
rebunee <zihna-19@hotmail.com>
2021-12-28 15:55:04 +0200

    navigating to the webstie and asserting the login url

--- src/test/java/NopCommerceTestingPkg/NopCommerceTestingCls.java
+++ src/test/java/NopCommerceTestingPkg/NopCommerceTestingCls.java
@@ -1,9 +1,23 @@
 package NopCommerceTestingPkg;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
 public class NopCommerceTestingCls {
 
-	public static void main(String[] args) {
+	static public WebDriver driver = new ChromeDriver();
 
+	public static void main(String[] args) {
+		driver.get("https://admin-demo.nopcommerce.com/");
+		assertPageUrl("https://admin-demo.nopcommerce.com/login?ReturnUrl=%2Fadmin%2F");
+
+		driver.close();
+	}
+
+	public static void assertPageUrl(String expectedUrl) {
+		String URL = driver.getCurrentUrl();
+		Assert.assertEquals(URL, expectedUrl);
 	}
 
 }
