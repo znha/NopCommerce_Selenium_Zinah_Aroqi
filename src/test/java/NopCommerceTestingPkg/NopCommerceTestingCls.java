508e7b9a5eed95b7b805b7d367f99fd7ce78d397
rebunee <zihna-19@hotmail.com>
2021-12-29 06:50:52 +0200

    assertion of categoiries and adding a sleep

--- src/test/java/NopCommerceTestingPkg/NopCommerceTestingCls.java
+++ src/test/java/NopCommerceTestingPkg/NopCommerceTestingCls.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 
 public class NopCommerceTestingCls {
@@ -12,68 +13,72 @@ public class NopCommerceTestingCls {
 	static public WebDriver driver = new ChromeDriver();
 	static public Actions actionProvider = new Actions(driver);
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		driver.get("https://admin-demo.nopcommerce.com/");
 		driver.manage().window().maximize();
-		
+
 		assertPageUrl("https://admin-demo.nopcommerce.com/login?ReturnUrl=%2Fadmin%2F");
 		assertPageTitle("Your store. Login");
 
 		Boolean loginButtonExists = driver.findElements(By.className("login-button")).size() > 0;
 		Assert.assertTrue(loginButtonExists);
-		
+
 		Boolean emailNotEmpty = !driver.findElement(By.id("Email")).getAttribute("value").equals("");
 		Assert.assertTrue(emailNotEmpty);
 
 		Boolean passwordNotEmpty = !driver.findElement(By.id("Password")).getAttribute("value").equals("");
 		Assert.assertTrue(passwordNotEmpty);
-		
+
 		WebElement rememberMeCheckbox = driver.findElement(By.id("RememberMe"));
 		rememberMeCheckbox.click();
 		Assert.assertTrue(rememberMeCheckbox.isSelected());
-		
+
 		WebElement loginButton = driver.findElement(By.className("login-button"));
 		actionProvider.moveToElement(loginButton).build().perform();
 		String loginButtonHover = loginButton.getCssValue("background-color");
-		Assert.assertEquals("rgba(36, 142, 206, 1)",loginButtonHover);
-		
+		Assert.assertEquals("rgba(36, 142, 206, 1)", loginButtonHover);
+
 		loginButton.click();
-		
+
 		assertPageUrl("https://admin-demo.nopcommerce.com/admin/");
 		assertPageTitle("Dashboard / nopCommerce administration");
 		assertPageHeading("Dashboard");
-		
-		Boolean checkUserName = driver.findElement(By.xpath("//div[@id=\"navbarText\"]/ul/li[2]")).getText().equals("John Smith");
+
+		Boolean checkUserName = driver.findElement(By.xpath("//div[@id=\"navbarText\"]/ul/li[2]")).getText()
+				.equals("John Smith");
 		Assert.assertTrue(checkUserName);
-		
-		Boolean checkSidebarVisible = !driver.findElement(By.className("os-content-glue")).getCssValue("width").equals("73px");
+
+		Boolean checkSidebarVisible = !driver.findElement(By.className("os-content-glue")).getCssValue("width")
+				.equals("73px");
 		Assert.assertTrue(checkSidebarVisible);
 		checkActiveNavItem("ul.nav a[href=\"/Admin\"]");
-		 
+
 		WebElement catalogLink = driver.findElement(By.xpath("//nav/ul/li[2]"));
-		
+
 //			  TODO - assert hover over Catalog // Nothing really seams to happen on the dom. needs mor investagtion
 
 		catalogLink.click();
 		hasClass(catalogLink, "menu-open");
-		Boolean catalogSubMenuIsVisible = driver.findElement(By.xpath("//nav/ul/li[2]/ul")).getCssValue("display").equals("block");
+		Boolean catalogSubMenuIsVisible = driver.findElement(By.xpath("//nav/ul/li[2]/ul")).getCssValue("display")
+				.equals("block");
 		Assert.assertTrue(catalogSubMenuIsVisible);
-		
+
 		WebElement porductsLink = driver.findElement(By.cssSelector("ul.nav a[href=\"/Admin/Product/List\"]"));
 		actionProvider.moveToElement(porductsLink).build().perform();
 		String productLinkHover = porductsLink.getCssValue("color");
-		Assert.assertEquals("rgba(255, 255, 255, 1)",productLinkHover);
-		
+		Assert.assertEquals("rgba(255, 255, 255, 1)", productLinkHover);
+
 		porductsLink.click();
 		assertPageUrl("https://admin-demo.nopcommerce.com/Admin/Product/List");
 		assertPageTitle("Products / nopCommerce administration");
 		assertPageHeading("Products");
 		checkActiveNavItem("ul.nav a[href=\"/Admin/Product/List\"]");
-		
+
 		WebElement addNewProductBtn = driver.findElement(By.cssSelector("a[href=\"/Admin/Product/Create\"]"));
 		actionProvider.moveToElement(addNewProductBtn).build().perform();
 		String addNewProductBtnHover = addNewProductBtn.getCssValue("background-color");
-		//Assert.assertEquals("rgba(70, 126, 159, 1)",addNewProductBtnHover); // it evaluates to tow different values ??
+		// Assert.assertEquals("rgba(70, 126, 159, 1)",addNewProductBtnHover); // it
+		// evaluates to tow different values ??
 
 		addNewProductBtn.click();
 		assertPageUrl("https://admin-demo.nopcommerce.com/Admin/Product/Create");
@@ -82,16 +87,15 @@ public class NopCommerceTestingCls {
 
 		WebElement modeChekbox = driver.findElement(By.cssSelector(".onoffswitch-inner"));
 		boolean isBasic = !modeChekbox.getCssValue("margin-left").equals(0);
-		if(!isBasic) {
+		if (!isBasic) {
 			modeChekbox.click();
 		}
-		
-		
+
 		Boolean productInfoCardIsCollapsed = checkCardIsCollapsed("product-info");
-		if(productInfoCardIsCollapsed) {
+		if (productInfoCardIsCollapsed) {
 			driver.findElement(By.id("product-info")).click();
 		}
-		
+
 		WebElement productName = driver.findElement(By.id("Name"));
 		String fristProduct = "first Prodcut By zinah";
 		productName.sendKeys(fristProduct);
@@ -102,12 +106,13 @@ public class NopCommerceTestingCls {
 		String shortDescriptionText = "first Prodcut short description By zinah";
 		shortDescription.sendKeys(shortDescriptionText);
 		Assert.assertEquals(shortDescription.getAttribute("value"), shortDescriptionText);
-		
-		
-		
+
 //			  - assert hovering over the ? button
 //
+//		WebElement frame = new WebDriverWait(driver, Duration.ofSeconds(3))
+//				.until(driver.findElement(By.id("FullDescription_ifr")));
 		
+		Thread.sleep(5000);
 		driver.switchTo().frame("FullDescription_ifr");
 		WebElement fullDescription = driver.findElement(By.id("tinymce"));
 		String fullDescriptionText = "first Prodcut full description By zinah";
@@ -115,7 +120,6 @@ public class NopCommerceTestingCls {
 		Assert.assertEquals(fullDescription.getText(), fullDescriptionText);
 		driver.switchTo().defaultContent();
 
-		
 //
 		WebElement skuInput = driver.findElement(By.id("Sku"));
 		String sku = "R2345678";
@@ -124,7 +128,9 @@ public class NopCommerceTestingCls {
 //			  - assert hovering over the ? button
 //
 //			- Fill the Categories field.
-//
+		Select categoriesSelect = new Select(driver.findElement(By.id("SelectedCategoryIds")));
+		categoriesSelect.selectByValue("1");
+
 //			  - assert filling the categories field.
 //			  - assert hovering over the ? button
 //
@@ -263,7 +269,7 @@ public class NopCommerceTestingCls {
 //			    - assert the windows is closed.
 //			    - assert the applied products table is filled with the selected product.
 
-		//driver.close();
+		// driver.close();
 	}
 
 	public static void assertPageUrl(String expectedUrl) {
@@ -298,12 +304,12 @@ public class NopCommerceTestingCls {
 		return false;
 
 	}
-	
+
 	public static Boolean checkCardIsCollapsed(String cardId) {
-		
+
 		WebElement elem = driver.findElement(By.id(cardId));
-		return  hasClass(elem, "collapsed-card");
-		
+		return hasClass(elem, "collapsed-card");
+
 	}
 
 }
